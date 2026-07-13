ea90572e8fd48d29b7ca4a05fbad6ae277c8f3b8
DeepshikhaSahoo9981 <deepshikha.sahoo.9981@gmail.com>
2026-07-13 18:02:51 +0530

    Portfolio prototype update - change screen resolution

--- frontend/portfolio/src/components/Header/Logo.jsx
+++ frontend/portfolio/src/components/Header/Logo.jsx
@@ -4,11 +4,11 @@ export default function Logo({ }) {
     return (
         <div className="Logo">
             <span className="common-text">De</span>
-            <span className="logo-container">
+            <div className="logo-container">
                 <span className="logo-text text-1">{names[0]}</span>
                 <span className="logo-text text-2">{names[1]}</span>
                 <span className="logo-text text-3">{names[2]}</span>
-            </span>
+            </div>
         </div>
     )
 }
